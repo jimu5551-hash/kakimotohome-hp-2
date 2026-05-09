1ba95dce2e55e3763ec145fb7a6b51b2d9c07bd6
Kakimoto-Jimu <jimu5551@gmail.com>
2026-05-09 11:05:31 +0900

    Update news text for 2026/04/11

--- src/components/News.js
+++ src/components/News.js
@@ -26,7 +26,7 @@ export const News = () => {
                 </svg>
               </div>
               <div class="news-date">2026/04/11</div>
-              <div class="news-title">施工事例写真を追加しました</div>
+              <div class="news-title">【リフォーム】キッチン収納 施工事例を追加しました</div>
             </a>
 
             <a href="/gallery.html#reform" class="news-item">
